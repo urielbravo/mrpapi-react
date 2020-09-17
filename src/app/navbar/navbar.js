52b282cbc5e557b5bd688d623c2cd1857c3ea394
Uriel Bravo <uriel@gmail.com>
2020-09-17 08:48:50 -0700

    Starting the project

---
+++ src/app/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function navbar() {
+  return (
+    <Container fluid>
+      <Row>
+        <col>hamburguer</col>
+        <col>Juan engineer</col>
+      </Row>
+    </Container>
+  );
+}
